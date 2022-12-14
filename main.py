4b5ca739909b8b773046b14865c0bb26d9124a9c
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2022-12-14 21:48:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ def main():
         n_node = 37484
 
 
-    #model = trans_to_cuda(SelfAttentionNetwork(opt, n_node))
-    model = trans_to_cuda(flash_cosine_sim_attention(opt, n_node))
+    model = trans_to_cuda(SelfAttentionNetwork(opt, n_node))
+    #model = trans_to_cuda(flash_cosine_sim_attention(opt, n_node))
    
     start = time.time()
     best_result = [0, 0]
